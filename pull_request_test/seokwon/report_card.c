0b422bdd5823d9b7c3685a6a812d263fc3d1fd11
hongseokwon,seok skkusosc <sw921111@gmail.com>
2018-04-10 19:10:51 +0900

    test pull request
    Signed-off-by: hongseokwon,seok skkusosc <sw921111@gmail.com>

--- pull_request_test/seokwon/report_card.c
+++ pull_request_test/seokwon/report_card.c
@@ -5,7 +5,7 @@ int main()
 	int kor, eng, math;
 	int sum = 0;
 
-	math = 80;
+	math = 90;
 	eng = 100;
 	kor = 90;
 	sum = 80 + 100 + 90;
@@ -16,6 +16,6 @@ int main()
 	printf("English : %d\n", eng);
 	printf("Math : %d\n", math);
 	printf("Sum : %d\n", sum);
-	printf("Mean : %d\n", sum/3);
+	printf("Average : %d\n", sum/3);
 	return 0;
 }
