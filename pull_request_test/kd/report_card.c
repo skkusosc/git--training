6ed70a060e5fa52b232db37625e48551f57a7dbd
sinkyoungdeok <tlsrudejr123@nate.com>
2019-04-09 01:56:45 -0700

    test pull request
    Signed-off-by: sinkyoungdeok <tlsrudejr123@nate.com>

---
+++ pull_request_test/kd/report_card.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int kor, eng, math;
+	int sum = 0;
+	int Average; 
+	math = 80;
+	eng = 100;
+	kor = 90;
+	sum = 80 + 100 + 90;
+	Average = sum/3;
+	printf("This program print report card.\n");
+
+	printf("Korean : %d\n", kor);
+	printf("English : %d\n", eng);
+	printf("Math : %d\n", math);
+	printf("Sum : %d\n", sum);
+	printf("Mean : %d\n", Average);
+	return 0;
+}
